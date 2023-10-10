32b498e28ef18d733f755ef97c713aee73319bf3
Zhiyue Chen <20301050198@fudan.edu.cn>
2023-10-10 22:08:11 +0800

    docs: update animation json

--- docs/src/.vuepress/theme/components/Home.vue
+++ docs/src/.vuepress/theme/components/Home.vue
@@ -46,7 +46,7 @@ export default defineComponent({
   data(){
       return{
           homeOptions: {
-              path: 'https://static.igem.wiki/teams/4765/wiki/czy/wiki-home-1.json',
+              path: 'https://static.igem.wiki/teams/4765/wiki/czy/wiki-3.json',
               loop: false,
               autoplay:true,
           },
